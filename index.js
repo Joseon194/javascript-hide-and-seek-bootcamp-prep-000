54de19a9e0797b5b5b8bd61cecfcddd7e12f7621
Dennis Chung <chungdennis6@gmail.com>
2019-01-20 15:10:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget() {
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+  }
+}
