655adef10d6e3dd83f699b01007a5093325428df
Dennis Chung <chungdennis6@gmail.com>
2019-01-20 15:13:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function increaseRankBy(n) {
     lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
 }
+
+function deepestChild() {
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=o;i<lis.length-1;i++) {
+    test=lis(i).querySelector("div");
+  }
+  return test
+}
